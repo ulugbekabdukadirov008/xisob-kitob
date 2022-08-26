3312544cd6ee6061133b39d3233ac741503ab6e4
AbdukadirovUlugbek <abdukadirovulugbek08@gmail.com>
2022-08-25 22:51:14 -1200

    xisob-kitob

---
+++ js/scrip.js
@@ -0,0 +1,13 @@
+alert("Xush kelibsiz")
+var firstName = prompt("Ismingizni kiriting:", "" )  
+var surName = prompt("Familiyangizni kiriting:", "")
+var  age = prompt("Yoshgizni kiriting", "")
+
+var birthYear = 2022 - age 
+var month = 12 * age
+var week = 52 * age
+var day = 365 * age 
+var hour = 5110 * 24 
+var second =  122640 * 60
+
+alert(`Hurmatli ${firstName}, ${surName}, Sizning yoshingiz ${age}da, Siz ${birthYear}-yili tug'ilgansiz, Siz ${age} yil, ${month} oy, ${week} hafta, ${day} kun, ${hour} soat, ${second} sekund yashadingiz. Buyog'i oz qoldi harakatizi qilo'rin. `)
